a93b056410318e1c7d67810efe40933a5737ad29
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-02-06 02:38:12 +0000

    Setup JMS to send messages when processing uploaded slides.

--- ritzalam/bigbluebutton-web/grails-app/conf/spring/resources.groovy
+++ ritzalam/bigbluebutton-web/grails-app/conf/spring/resources.groovy
@@ -1,5 +1,18 @@
 // Place your Spring DSL code here
 beans = {
+
+	jmsFactory(org.apache.activemq.pool.PooledConnectionFactory) { bean ->
+		bean.destroyMethod = "stop"
+		connectionFactory = {org.apache.activemq.ActiveMQConnectionFactory cf ->
+			brokerURL = "tcp://localhost:61616"
+		}
+	}
+	
+	jmsTemplate(org.springframework.jms.core.JmsTemplate) {
+		connectionFactory = jmsFactory
+	}
+	
+	
 //		   pbxLive(org.bigbluebutton.pbx.PbxLive) { bean ->
 //			      bean.initMethod = "startup" 
 //			      bean.destroyMethod = "shutdown"
