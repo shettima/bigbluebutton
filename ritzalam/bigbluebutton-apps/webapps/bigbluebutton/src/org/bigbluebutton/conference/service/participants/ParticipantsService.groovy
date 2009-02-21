f7e65cebba923990f9c3b9d3272ab2c58167a917
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-02-21 16:36:05 +0000

    Restructure participants.

--- ritzalam/bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/participants/ParticipantsService.groovy
+++ ritzalam/bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/participants/ParticipantsService.groovy
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory
 import org.red5.server.api.so.ISharedObject
 import org.red5.server.adapter.ApplicationAdapter
 import org.red5.server.api.Red5import java.util.Mapimport org.bigbluebutton.conference.RoomsManager
-import org.bigbluebutton.conference.Room
+import org.bigbluebutton.conference.Roomimport org.bigbluebutton.conference.Participant
 public class ParticipantsService extends ApplicationAdapter implements IApplication{
 
 	protected static Logger log = LoggerFactory.getLogger( ParticipantsService.class );
@@ -25,69 +25,68 @@ public class ParticipantsService extends ApplicationAdapter implements IApplicat
 	
 	@Override
 	public boolean appConnect(IConnection conn, Object[] params) {
-		println "${APP}:appConnect"
-		return true;
+		log.debug("${APP}:appConnect")
+		return true
 	}
 
 	@Override
 	public void appDisconnect(IConnection conn) {
-		println "${APP}:appDisconnect"
+		log.debug( "${APP}:appDisconnect")
 	}
 
 	@Override
 	public boolean appJoin(IClient client, IScope scope) {
-		println "${APP}:appJoin ${scope.name}"
-		return true;
+		log.debug( "${APP}:appJoin ${scope.name}")
+		return true
 	}
 
 	@Override
 	public void appLeave(IClient client, IScope scope) {
-		println "${APP}:appLeave ${scope.name}"
+		log.debug("${APP}:appLeave ${scope.name}")
 
 	}
 
 	@Override
 	public boolean appStart(IScope scope) {
-		println "${APP}:appStart ${scope.name}"
+		log.debug("${APP}:appStart ${scope.name}")
 		return true;
 	}
 
 	@Override
 	public void appStop(IScope scope) {
-		println "${APP}:appStop ${scope.name}"
+		log.debug("${APP}:appStop ${scope.name}")
 	}
 
 	@Override
 	public boolean roomConnect(IConnection connection, Object[] params) {
-		println "${APP}:roomConnect"
+		log.debug("${APP}:roomConnect")
 
     	return true;
 	}
 
 	@Override
 	public void roomDisconnect(IConnection connection) {
-		println "${APP}:roomDisconnect"
+		log.debug("${APP}:roomDisconnect")
 
 	}
 
 	@Override
 	public boolean roomJoin(IClient client, IScope scope) {
-		println "${APP}:roomJoin ${scope.name} - ${scope.parent.name}"
+		log.debug("${APP}:roomJoin ${scope.name} - ${scope.parent.name}")
 		participantJoin();
 		return true;
 	}
 
 	@Override
 	public void roomLeave(IClient client, IScope scope) {
-		println "${APP}:roomLeave ${scope.name}"
+		log.debug("${APP}:roomLeave ${scope.name}")
 		participantLeft()
 	}
 
 	@Override
 	public boolean roomStart(IScope scope) {
-		println "${APP} - roomStart ${scope.name}"
-    	log.info( "${APP} - roomStart ${scope.name}" )
-		
+		log.debug("${APP} - roomStart ${scope.name}")
+
     	// create ParticipantSO if it is not already created
     	if (!hasSharedObject(scope, PARTICIPANTS_SO)) {
     		createSharedObject(scope, PARTICIPANTS_SO, false)
@@ -97,23 +96,38 @@ public class ParticipantsService extends ApplicationAdapter implements IApplicat
 
 	@Override
 	public void roomStop(IScope scope) {
-		println "${APP}:roomStop ${scope.name}"
+		log.debug("${APP}:roomStop ${scope.name}")
 		if (!hasSharedObject(scope, PARTICIPANTS_SO)) {
     		clearSharedObjects(scope, PARTICIPANTS_SO)
     	}
 	}
 	
 	public Map getParticipants() {
-		println "${APP}:getParticipants"
+		log.debug("${APP}:getParticipants")
 		Room room = roomsManager.getRoom(Red5.connectionLocal.scope.name)
 		
 		Map participants = new HashMap()
 		participants.put("count", room.numberOfParticipants)
 		if (room.numberOfParticipants > 0) {
-			participants.put("participants", room.participants)
+			/**
+			 * Somehow we need to convert to Map so the client will be
+			 * able to decode it. Need to figure out if we can send Participant
+			 * directly. (ralam - 2/20/2009)
+			 */
+			Collection pc = room.participants.values()
+    		Map pm = new HashMap()
+    		for (Iterator it = pc.iterator(); it.hasNext();) {
+    			Participant ap = (Participant) it.next();
+    			pm.put(ap.userid, ap.toMap()); 
+    		}  
+			participants.put("participants", pm)
 		}
-		println "${APP}:getParticipants " + participants.get("count")
+		log.debug("${APP}:getParticipants " + participants.get("count"))
 		return participants
+	}
+	
+	public boolean assignPresenter(Integer userid, Integer assignedBy) {
+		log.debug("Assigning presenter to ${userid} by ${assignedBy}")
 	}
 	
 	public boolean participantLeft() {
@@ -134,25 +148,29 @@ public class ParticipantsService extends ApplicationAdapter implements IApplicat
 		ISharedObject so = getSharedObject(Red5.connectionLocal.scope, PARTICIPANTS_SO, false)
 		def userid = Red5.connectionLocal.client.id;
 		def username = Red5.connectionLocal.getAttribute("username")
-		
+		def role = Red5.connectionLocal.getAttribute("role")
+/*		
 		Map user = new HashMap()
 		user.put("userid", userid)
 		user.put("username", username)
 		user.put("role", Red5.connectionLocal.getAttribute("role"))
-		
+*/
+
 		Map status = new HashMap()
 		status.put("raiseHand", false)
 		status.put("presenter", true)
 		status.put("hasStream", false)
-		user.put("status", status)
+//		user.put("status", status)
+		
+		Participant p = new Participant(userid, username, role, status)
 		
 		Room room = roomsManager.getRoom(Red5.connectionLocal.scope.name)
-		room.addParticipant(user)
+		room.addParticipant(p)
 		
-		println "${APP}:participantJoin " + room.participants.size 
+		log.debug("${APP}:participantJoin " + room.participants.size) 
 		
 		List args = new ArrayList()
-		args.add(user)
+		args.add(p.toMap())
 		
 		recorder.debug("${APP} - ParticipantJoinEvent - ${userid}")
 		
