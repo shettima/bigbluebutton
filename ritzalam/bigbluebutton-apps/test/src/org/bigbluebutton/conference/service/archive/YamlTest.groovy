8d124511e3482f858b371db61e8a9f6cef262494
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-03-08 03:23:43 +0000

    - Read and write events into a YAML file. - Restructure record package.

--- ritzalam/bigbluebutton-apps/test/src/org/bigbluebutton/conference/service/archive/YamlTest.groovy
+++ ritzalam/bigbluebutton-apps/test/src/org/bigbluebutton/conference/service/archive/YamlTest.groovy
@@ -2,52 +2,72 @@
 package org.bigbluebutton.conference.service.archive
 
 import org.testng.annotations.BeforeMethodimport org.testng.annotations.Testimport org.ho.yaml.Yaml
-
+import org.ho.yaml.YamlEncoderimport org.ho.yaml.YamlDecoder
 public class YamlTest{
 	File f 
+	File yamlOutFile
 	
 	@BeforeMethod
 	public void setUp() {
-		f = new File('test/resources/test.yaml')
+		f = new File('test/resources/recordings.yaml')
 		println f.absolutePath
+		File yamlOutFileDir = new File('build/test/resources')
+		if (! yamlOutFileDir.exists())
+			yamlOutFileDir.mkdirs()
+		yamlOutFile = new File('build/test/resources/yaml-output.yaml')
+		println yamlOutFile.canonicalPath
 	}
 
 	@Test
-	public void filepathTest() {
-		
+	public void writeYamlToFileTest() {
+		Map event = new HashMap()
+		event.put("date", 1236202122980)
+		event.put("application", "PARTICIPANT")
+		event.put("event", "ParticipantJoinedEvent")
+		Map status = new HashMap()
+		event.put("status", status)
+		status.put("raiseHand", true)
+		status.put("presenter", true)
+		status.put("stream", "my-video-stream")
+    
+		Map event1 = new HashMap()
+		event1.put("date", 1236202132980)
+		event1.put("application", "PARTICIPANT")
+		event1.put("event", "ParticipantJoinedEvent")
+		event1.put("message", "Las Vegas, Nevada, USA")
+
+		// We'll save multiple YAML document into the file. 
+		YamlEncoder enc = new YamlEncoder(yamlOutFile.newOutputStream());
+        enc.writeObject(event);
+        enc.writeObject(event1);
+        enc.close();
+        
+        // Let's read back the saved data.
+        YamlDecoder dec = new YamlDecoder(yamlOutFile.newInputStream());
+        def eventList = []
+        try{
+          while (true){
+            Map eventRead = dec.readObject();
+            println eventRead.date
+            eventList.add(eventRead)
+          }
+        }catch (EOFException e){
+          println("Finished reading stream.");
+        }finally {
+          dec.close();
+        }
+        
+        assert eventList.size() == 2
+		assert eventList[0].date == 1236202122980
+		assert eventList[0]['application'] == 'PARTICIPANT'
 	}
 	
 	@Test
 	public void simpleYamlTest() {
-			def player = Yaml.load(new File(f.absolutePath))
-			assert player[0].Birthplace == "Las Vegas, Nevada, USA"
-			assert player[0]['Turned Pro'] == 1986
-			assert player.size() == 5
-			println Yaml.dump(player)
-	}
-
-	public void anotherSipleYamlTest() {
-			def input = '''
-			---
-			time: 20:03:20
-			player: Sammy Sosa
-			action: strike (miss)
-			...
-			---
-			time: 20:03:47
-			player: Sammy Sosa
-			action: grand slam
-			...
-			'''
-
-			def play = Yaml.load(input)
-			print play
-			play = Yaml.load(input)
-			print play
-//			assert play.time == "20:03:47"
-//			assert player.player == "Sammy Sosa"
-
-
+			def eventRead = Yaml.load(new File(f.absolutePath))
+			assert eventRead[0].date == 1236202122980
+			assert eventRead[0]['application'] == 'PARTICIPANT'
+			assert eventRead.size() == 5
+			println Yaml.dump(eventRead)
 	}	
-	
 }
--- ritzalam/bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/archive/record/RecordService.groovy
+++ ritzalam/bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/archive/record/RecordService.groovy
@@ -1,4 +1,4 @@
-package org.bigbluebutton.conference.service.archive
+package org.bigbluebutton.conference.service.archive.record
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
