3928cc1734c2d16aa33f0fb3c2defea4625e4583
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-03-10 19:50:46 +0000

    - modify how we add application listeners to BigBlueButtonApplication - Create BigBlueButtonSession to store session data for a connection - Add more tests.

--- branches/developers/ritzalam/bigbluebutton-apps/test/src/org/bigbluebutton/conference/service/archive/recorder/RecordSessionTest.groovy
+++ branches/developers/ritzalam/bigbluebutton-apps/test/src/org/bigbluebutton/conference/service/archive/recorder/RecordSessionTest.groovy
@@ -1,7 +1,7 @@
 
 package org.bigbluebutton.conference.service.archive.recorder
 
-import org.testng.annotations.BeforeMethodimport org.testng.annotations.Testimport org.bigbluebutton.conference.service.archive.record.RecordSessionimport org.bigbluebutton.conference.service.archive.record.IRecorderimport org.bigbluebutton.conference.service.archive.record.IEventRecorder
+import org.testng.annotations.BeforeMethodimport org.testng.annotations.Testimport org.bigbluebutton.conference.service.archive.record.RecordSessionimport org.bigbluebutton.conference.service.archive.record.IRecorderimport org.bigbluebutton.conference.service.archive.record.IEventRecorderimport org.testng.Assert
 public class RecordSessionTest{
 	RecordSession session
 	
@@ -12,17 +12,7 @@ public class RecordSessionTest{
 
 	@Test
 	public void recordSessionTest() {
-		Map event1 = new HashMap()
-		event1.put("date", 1236202132980)
-		event1.put("application", "PARTICIPANT")
-		event1.put("event", "ParticipantJoinedEvent")
-		event1.put("message", "Las Vegas, Nevada, USA")
-		
-		def sampleEventRecorder = {
-				assert event1 == it
-		}
-		def r = [recordEvent:sampleEventRecorder] as IRecorder
-		session.setRecorder(r)
+		// Mock the IEventRecorder interface
 		def recorderPassedFromRecordSession
 		def eventRecorderMockAcceptRecorderMethod = {
 			recorderPassedFromRecordSession = it
@@ -32,6 +22,23 @@ public class RecordSessionTest{
 		}
 		
 		def eventRecorderMock = [acceptRecorder:eventRecorderMockAcceptRecorderMethod, recordEvent:eventRecorderMockRecordEventMethod] as IEventRecorder
+		
+		// Setup our test event to be recorded
+		Map event1 = new HashMap()
+		event1.put("date", 1236202132980)
+		event1.put("application", "PARTICIPANT")
+		event1.put("event", "ParticipantJoinedEvent")
+		event1.put("message", "Las Vegas, Nevada, USA")
+		
+		// Mock the IRecorder interface
+		def sampleEventRecorder = {
+				Assert.assertEquals(event1, it, "The returned event shoule be the same as the one that we sent.")
+		}
+		def r = [recordEvent:sampleEventRecorder] as IRecorder
+		
+		// Use the mocked recorder in our session
+		session.setRecorder(r)
+		
 		session.addEventRecorder(eventRecorderMock)
 		eventRecorderMock.recordEvent(event1)
 	}		
