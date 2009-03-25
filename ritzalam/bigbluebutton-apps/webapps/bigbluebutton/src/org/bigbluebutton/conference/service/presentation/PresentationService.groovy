864cd8d1a4cb9158e27a77f6329518c69cfb089d
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-03-25 19:51:01 +0000

    - fix assigning presenter to display the correct presenter name - if there is already a presenter, change the status of that user as a normal user and assign the new presenter

--- ritzalam/bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/presentation/PresentationService.groovy
+++ ritzalam/bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/presentation/PresentationService.groovy
@@ -9,22 +9,26 @@ public class PresentationService {
 	private ParticipantsApplication participantsApplication
 	private PresentationApplication presentationApplication
 
-	public void assignPresenter(String userid, String name, String assignedBy) {
+	def assignPresenter(userid, name, assignedBy) {
 		log.debug("assignPresenter $userid $name $assignedBy")
 		IScope scope = Red5.connectionLocal.scope
 		def presenter = [userid, name, assignedBy]
 		def curPresenter = presentationApplication.getCurrentPresenter(scope.name)
 		participantsApplication.setParticipantStatus(scope.name, userid.toLong(), "presenter", true)
+		if (curPresenter != null && (curPresenter[0] != userid)) {
+			log.debug("Changing presenter from ${curPresenter[0]} to $userid")
+			participantsApplication.setParticipantStatus(scope.name, curPresenter[0].toLong(), "presenter", false)
+		}
 		presentationApplication.assignPresenter(scope.name, presenter)
 	}
 	
-	public void gotoSlide(def slideNum) {
+	public void gotoSlide(slideNum) {
 		log.debug("Request to go to slide $slideNum")
 		IScope scope = Red5.connectionLocal.scope
 		presentationApplication.gotoSlide(scope.name, slideNum)
 	}
 	
-	public void sharePresentation(def presentationName, def share) {
+	public void sharePresentation(presentationName, share) {
 		log.debug("Request to go to sharePresentation $presentationName $share")
 		IScope scope = Red5.connectionLocal.scope
 		presentationApplication.sharePresentation(scope.name, presentationName, share)
