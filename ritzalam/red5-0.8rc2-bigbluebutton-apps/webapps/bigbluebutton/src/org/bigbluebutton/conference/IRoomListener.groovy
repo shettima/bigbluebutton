0775797bca2f69a61bafd7dbaa85cfc310e55d08
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-04-01 20:40:59 +0000

    - the branch earlier on was missing some apps

--- ritzalam/red5-0.8rc2-bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/IRoomListener.groovy
+++
@@ -1,9 +0,0 @@
-
-package org.bigbluebutton.conference
-
-public interface IRoomListener {
-
-	public void participantStatusChange(String userid, String status, Object value);
-	public void participantJoined(Participant participant);
-	public void participantLeft(String userid);
-}
