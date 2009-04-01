9efe6d9c475790ac3b3a76ce50edce27bccd3039
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-04-01 20:40:59 +0000

    - the branch earlier on was missing some apps

--- branches/developers/ritzalam/red5-0.8rc2-bigbluebutton-apps/test/org/bigbluebutton/conference/service/participants/ParticipantsApplicationTest.groovy
+++
@@ -1,32 +0,0 @@
-
-package org.bigbluebutton.conference.service.participants
-
-import org.testng.annotations.*
-import org.bigbluebutton.conference.RoomsManagerimport org.bigbluebutton.conference.Roomimport org.bigbluebutton.conference.Participantimport org.testng.Assert
-class ParticipantsApplicationTest{
-	RoomsManager rm
-	Room r
-	Participant p
-	ParticipantsApplication pa
-	
-	@BeforeTest
-	public void setUp() {
-	    rm = new RoomsManager()
-	    r = new Room("test-room")
-		Map status = new HashMap()
-		status.put("raiseHand", false)
-		status.put("presenter", false)
-		status.put("hasStream", false)
-		p = new Participant(1.toString(), 'Test User', 'MODERATOR', status)
-	    r.addParticipant(p)
-	    rm.addRoom(r)
-	    pa.setRoomsManager(rm)
-	}
-
-	@Test
-	public void getParticipantsTest() {
-		Map pm = pa.getParticipants("test-room")
-	    Assert.assertTrue(pm.get("count") == 1, "There should only be one participants in the room.")
-	}
-	
-}
