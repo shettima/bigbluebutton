8bce5ad7406ce854d24d11608395ef17297146ea
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-04-17 14:26:55 +0000

    - branch apps for extending playback of recording

---
+++ ritzalam/bbb-apps-playback/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/archive/record/FileRecorder.groovy
@@ -0,0 +1,70 @@
+
+package org.bigbluebutton.conference.service.archive.record
+
+import org.ho.yaml.YamlEncoderimport org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.red5.logging.Red5LoggerFactory
+
+public class FileRecorder implements IRecorder{
+	private static Logger log = Red5LoggerFactory.getLogger( FileRecorder.class, "bigbluebutton" )
+	
+	private final String conference
+	private final String room
+	private final String recordingsDirectory
+	private final File recordingFile
+	private final File roomDir
+	
+	public FileRecorder(String conference, String room) {
+		this.conference = conference
+		this.room = room
+	}
+	
+	public void initialize() {
+		roomDir = new File("$recordingsDirectory/$conference/$room")
+		if (! roomDir.exists())
+			roomDir.mkdirs()
+		recordingFile = new File(roomDir.canonicalPath + File.separator + "recordings.xml" )
+		/**
+		 * We do not actually want to delete the file. We just want to append to it.
+		 */
+		 //deleteRecording()
+	}
+	
+	public void deleteRecording() {
+		if (recordingFile.exists()) {
+			// delete the file so we start fresh
+			recordingFile.delete()
+			recordingFile = new File(roomDir.canonicalPath + File.separator + "recordings.yaml" )
+		}		
+	}
+	
+	public void recordEvent(Map event) {
+		log.debug("Recording event to file ${recordingFile.absolutePath}.")
+		FileOutputStream fout = new FileOutputStream(recordingFile, true /*append*/)
+		log.debug("Recording event to file - got output stream.")
+		if (fout == null) {
+			log.debug("Outputstream is null")
+		} else {
+			log.debug("Outputstream is NOT null")
+		}
+		
+		Thread.start {
+			YamlEncoder enc = new YamlEncoder(fout)
+			log.debug("Recording event to file - setting up encoder.")
+	        enc.writeObject(event)
+	        log.debug("Recording event to file - writing the event to file.")
+	        enc.close()  
+	        log.debug("Recorded event to file - closed encoder.")
+		}        
+	}
+	
+	def recordXmlEvent(event) {		
+		Thread.start {
+			recordingFile.append(event)
+		}  
+	}
+	
+	public void setRecordingsDirectory(String directory) {
+		recordingsDirectory = directory
+	}
+}
