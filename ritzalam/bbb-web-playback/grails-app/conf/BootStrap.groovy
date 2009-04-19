27ba3f052f1337eadb2053e1c095409e54e494e5
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-04-19 21:14:08 +0000

    - make logging work for grails artefacts

--- ritzalam/bbb-web-playback/grails-app/conf/BootStrap.groovy
+++ ritzalam/bbb-web-playback/grails-app/conf/BootStrap.groovy
@@ -7,6 +7,7 @@ class BootStrap {
 	def jmsContainer
 	
      def init = { servletContext ->
+     	log.debug "Bootstrapping"
      	// Administrator user and role.
 		def adminRole = new Role(name: "Administrator").save()
 		def adminUser = new User(username: "admin", passwordHash: new Sha1Hash("admin").toHex(),
