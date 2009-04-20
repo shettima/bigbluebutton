7a3099ff5b6a2e5e8a25e90ead7b36581aec5652
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-04-20 01:35:44 +0000

    - put in sample presentation into test/resources to be used for testing

--- ritzalam/bbb-web-playback/test/integration/org/bigbluebutton/web/services/PresentationServiceTests.groovy
+++ ritzalam/bbb-web-playback/test/integration/org/bigbluebutton/web/services/PresentationServiceTests.groovy
@@ -3,6 +3,7 @@ package org.bigbluebutton.web.services
 class PresentationServiceTests extends GroovyTestCase {
 
     void testSomething() {
-
+    	def file = new File('test/resources/sample-presentation.pdf')
+    	println "FIle path =${file.absolutePath}"
     }
 }
