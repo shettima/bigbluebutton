ef996dd8f6d208335aec83e0854b60fc1d254035
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-04-20 01:50:44 +0000

    - setup PresentationService tests

--- ritzalam/bbb-web-playback/test/integration/org/bigbluebutton/web/services/PresentationServiceTests.groovy
+++ ritzalam/bbb-web-playback/test/integration/org/bigbluebutton/web/services/PresentationServiceTests.groovy
@@ -1,9 +1,24 @@
 package org.bigbluebutton.web.services
 
 class PresentationServiceTests extends GroovyTestCase {
-
-    void testSomething() {
+	
+	def presService
+	
+	void setUp() {
+		presService = new PresentationService()
+	}
+	
+    void testProcessGoodPresentation() {
     	def file = new File('test/resources/sample-presentation.pdf')
     	println "FIle path =${file.absolutePath}"
+    	presService.processUploadedPresentation("test-conf", "test-room", "test-presentation", file)
+    }
+    
+    void testProcessOneSlideWithTooManyObjectsPresentation() {
+    	
+    }
+    
+    void testProcessSeveralSlidesWithTooManyObjectsPresentation() {
+    	
     }
 }
