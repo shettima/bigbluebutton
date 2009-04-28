8b83026e139686bd86eb76274281457d9f5e6490
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-04-28 03:04:00 +0000

    - refactor upload presentationservice

--- branches/developers/ritzalam/bbb-web-playback/test/integration/org/bigbluebutton/web/services/PresentationServiceTests.groovy
+++ branches/developers/ritzalam/bbb-web-playback/test/integration/org/bigbluebutton/web/services/PresentationServiceTests.groovy
@@ -1,17 +1,111 @@
 package org.bigbluebutton.web.services
 
+import org.springframework.util.FileCopyUtils
+
 class PresentationServiceTests extends GroovyTestCase {
 	
-	def presService
+	def SWFTOOLS = "C:/swftools-0.9"
+	def IMAGEMAGICK = "C:/ImageMagick-6.4.8-Q16/"
+	def GHOSTSCRIPT = "C:/gs/gs8.63/bin/gswin32c.exe"
+	def PRESENTATIONDIR = "d:/temp/bigbluebutton"
 	
+	def presService
+		
 	void setUp() {
 		presService = new PresentationService()
+		presService.swfTools = SWFTOOLS
+		presService.imageMagick = IMAGEMAGICK
+		presService.ghostScript = GHOSTSCRIPT
+		presService.presentationDir = PRESENTATIONDIR
 	}
 	
+	void testGetUploadDirectory() {
+		def uploadedFilename = 'sample-presentation.pdf'		
+		def uploadedFile = new File("test/resources/$uploadedFilename")
+    	def conf = "test-conf"
+    	def rm = "test-room"
+    	def presName = "test-presentation"
+    	    	
+		File uploadDir = presService.uploadedPresentationDirectory(conf, rm, presName)
+		def uploadedPresentation = new File(uploadDir.absolutePath + File.separator + uploadedFilename)
+    	uploadedPresentation = new File("$PRESENTATIONDIR/$conf/$rm/$presName/$uploadedFilename")
+    	int copied = FileCopyUtils.copy(uploadedFile, uploadedPresentation) 
+    	assertTrue(uploadedPresentation.exists())
+	}
+	
+	void testGetNumberOfPagesForPresentation() {
+		def uploadedFilename = 'sample-presentation.pdf'		
+		def uploadedFile = new File("test/resources/$uploadedFilename")
+	    def conf = "test-conf"
+	    def rm = "test-room"
+	    def presName = "test-presentation"
+	    	    	
+		File uploadDir = presService.uploadedPresentationDirectory(conf, rm, presName)
+		def uploadedPresentation = new File(uploadDir.absolutePath + File.separator + uploadedFilename)
+	    uploadedPresentation = new File("$PRESENTATIONDIR/$conf/$rm/$presName/$uploadedFilename")
+	    int copied = FileCopyUtils.copy(uploadedFile, uploadedPresentation) 
+	    assertTrue(uploadedPresentation.exists())
+		int numPages = presService.determineNumberOfPages(uploadedPresentation)
+		assertEquals 8, numPages
+	}
+		
+	void testConvertPage() {
+		def uploadedFilename = 'sample-presentation.pdf'		
+		def uploadedFile = new File("test/resources/$uploadedFilename")
+		def conf = "test-conf"
+		def rm = "test-room"
+		def presName = "test-presentation"
+		    	    	
+		File uploadDir = presService.uploadedPresentationDirectory(conf, rm, presName)
+		def uploadedPresentation = new File(uploadDir.absolutePath + File.separator + uploadedFilename)
+		uploadedPresentation = new File("$PRESENTATIONDIR/$conf/$rm/$presName/$uploadedFilename")
+		int copied = FileCopyUtils.copy(uploadedFile, uploadedPresentation) 
+		assertTrue(uploadedPresentation.exists())
+		int numPages = presService.determineNumberOfPages(uploadedPresentation)
+		assertEquals 8, numPages
+		
+		for (int page = 1; page <= numPages; page++) {
+			presService.convertUsingPdf2Swf(uploadedPresentation, page)
+		}	    
+	}
+
+	void testExtractPageUsingGhostscript() {
+		def uploadedFilename = 'sample-presentation.pdf'		
+		def uploadedFile = new File("test/resources/$uploadedFilename")
+		def conf = "test-conf"
+		def rm = "test-room"
+		def presName = "test-presentation"
+			    	    	
+		File uploadDir = presService.uploadedPresentationDirectory(conf, rm, presName)
+		def uploadedPresentation = new File(uploadDir.absolutePath + File.separator + uploadedFilename)
+		uploadedPresentation = new File("$PRESENTATIONDIR/$conf/$rm/$presName/$uploadedFilename")
+		int copied = FileCopyUtils.copy(uploadedFile, uploadedPresentation) 
+		assertTrue(uploadedPresentation.exists())
+		int numPages = presService.determineNumberOfPages(uploadedPresentation)
+		assertEquals 8, numPages
+			
+		for (int page = 1; page <= numPages; page++) {
+			presService.extractPageUsingGhostScript(uploadedPresentation, page)
+		}
+		
+		for (int page = 1; page <= numPages; page++) {
+			presService.convertUsingImageMagick(uploadedPresentation, page)
+		}
+	}
+	
+/*	
     void testProcessGoodPresentation() {
     	def file = new File('test/resources/sample-presentation.pdf')
     	println "FIle path =${file.absolutePath}"
-    	presService.processUploadedPresentation("test-conf", "test-room", "test-presentation", file)
+    	def conf = "test-conf"
+    	def rm = "test-room"
+    	def presName = "test-presentation"
+    	File uploadDir = presService.uploadedPresentationDirectory(conf, rm, presName)
+    	println uploadDir
+    	
+//    	presService.processUploadedPresentation(conf, rm, presName, file)
+		def numPages = presService.determineNumberOfPages(file)
+		println numPages
     }
     
     void testProcessOneSlideWithTooManyObjectsPresentation() {
@@ -21,4 +115,5 @@ class PresentationServiceTests extends GroovyTestCase {
     void testProcessSeveralSlidesWithTooManyObjectsPresentation() {
     	
     }
+*/
 }
