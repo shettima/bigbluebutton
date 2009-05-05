b71d4b2d7680f99c619d74ba5db6198bb98764fa
minoustudio <minoustudio@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-05-05 00:06:56 +0000

    this is genarated from 20090504's trunk which is a new version, the WhiteBoard code I checkin is tested with an older version, so I deleted it here and I will checked in that older version of bigbluebutton-apps

--- branches/developers/bo/bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/IRoomListener.groovy
+++
@@ -1,9 +0,0 @@
-
-package org.bigbluebutton.conference
-
-public interface IRoomListener {
-	def getName()
-	public void participantStatusChange(Long userid, String status, Object value);
-	public void participantJoined(Participant participant);
-	public void participantLeft(Long userid);
-}
