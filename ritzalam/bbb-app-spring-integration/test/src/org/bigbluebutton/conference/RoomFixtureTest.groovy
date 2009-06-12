9f5fc50ee4e3600a84320007808486866e312a8b
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2009-06-12 15:27:38 +0000

    - try setting up license and Id

--- ritzalam/bbb-app-spring-integration/test/src/org/bigbluebutton/conference/RoomFixtureTest.groovy
+++ ritzalam/bbb-app-spring-integration/test/src/org/bigbluebutton/conference/RoomFixtureTest.groovy
@@ -1,24 +1,23 @@
-
 /*
  * BigBlueButton - http://www.bigbluebutton.org
  * 
  * Copyright (c) 2008-2009 by respective authors (see below). All rights reserved.
  * 
- * This program is free software; you can redistribute it and/or modify it under the 
+ * BigBlueButton is free software; you can redistribute it and/or modify it under the 
  * terms of the GNU Lesser General Public License as published by the Free Software 
  * Foundation; either version 3 of the License, or (at your option) any later 
  * version. 
  * 
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * BigBlueButton is distributed in the hope that it will be useful, but WITHOUT ANY 
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
  * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License along 
- * with this library; if not, write to the Free Software Foundation, Inc., 
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ * with BigBlueButton; if not, If not, see <http://www.gnu.org/licenses/>.
  *
  * $Id$
  */
+ 
 package org.bigbluebutton.conference
 
 import fit.ColumnFixture
