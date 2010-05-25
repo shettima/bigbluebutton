795290308d0f19f8524ea003f6ce0a7cc57761f8
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2010-05-25 16:13:05 +0000

    - cleanup as we've moved the repositoy to github (http://github.com/bigbluebutton) so other won't accidentally use code from here

--- bbb-api-examples/PHP/demo_footer.php
+++
@@ -1,2 +0,0 @@
-<p/>
-<small>These demos use the <a href="http://code.google.com/p/bigbluebutton/wiki/API">BigBlueButton API</a>. The source code for these demos is available <a href="http://code.google.com/p/bigbluebutton/source/browse/#svn/trunk/bbb-api-examples/PHP/">here</a>.</small>
