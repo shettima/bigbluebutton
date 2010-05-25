db9f852b7b75a89651d14ff1f1909a1d5bde4940
ritzalam <ritzalam@af16638f-c34d-0410-8cfa-b39d5352b314>
2010-05-25 15:19:40 +0000

    - removing as we've moved the repositoy to github (http://github.com/bigbluebutton) so other won't accidentally use code from here

--- branches/developers/ritzalam/client-mate/bbb-app-spring-integration/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/archive/playback/PlaybackService.groovy
+++
@@ -1,9 +0,0 @@
-package org.bigbluebutton.conference.service.archive.playback
-
-public class PlaybackService implements IPlaybackService{
-
-	public void addPlaybackNotifier(String name, IPlaybackNotifier n) {
-		
-	}
-	
-}
--- branches/developers/ritzalam/client-playback/bbb-apps-build-cleanup/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/archive/playback/IPlayMessageNotifier.groovy
+++
@@ -1,9 +0,0 @@
-
-package org.bigbluebutton.conference.service.archive.playback
-
-
-public interface IPlayMessageNotifier{
-
-	public void playMessage(String message);
-	
-}
